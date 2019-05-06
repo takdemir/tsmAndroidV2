ec0431741b7630747b0a43ba4ad504fc387abbbd
Taner Akdemir <admin@tsmsoft.net>
2019-05-06 19:29:18 +0300

    target is decreased to 27

--- platforms/android/app/build/generated/source/buildConfig/debug/com/kuryeotomasyon/www/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/kuryeotomasyon/www/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.kuryeotomasyon.www";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 201905042;
+  public static final int VERSION_CODE = 201905043;
   public static final String VERSION_NAME = "0.0.4";
 }
